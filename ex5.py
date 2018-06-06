074885b758e1414f0c26219cba698bd1b1857ebf
ayeMoe <ayemoeaungcu@gmail.com>
2018-06-06 10:01:16 +0530

    added ex5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name ='Aye Moe Aung'
+my_age = 21
+my_height = 74
+my_weight = 120
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"She teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height +my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
